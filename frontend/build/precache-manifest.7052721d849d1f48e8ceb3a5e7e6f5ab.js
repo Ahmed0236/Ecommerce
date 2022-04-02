a0a3bf9681e0e1fa69857f39db6d9b02051c8df1
ahmed0236 <ahmedanwar5295@gmail.com>
2022-04-02 17:16:19 -0400

    Production Ready

---
+++ frontend/build/precache-manifest.7052721d849d1f48e8ceb3a5e7e6f5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d81d8d16b2507fef0b",
+    "url": "/static/css/main.72f27973.chunk.css"
+  },
+  {
+    "revision": "46d81d8d16b2507fef0b",
+    "url": "/static/js/main.46d81d8d.chunk.js"
+  },
+  {
+    "revision": "655ced442bf892c5e859",
+    "url": "/static/js/1.655ced44.chunk.js"
+  },
+  {
+    "revision": "7643751645daeab7f6b7",
+    "url": "/static/js/2.76437516.chunk.js"
+  },
+  {
+    "revision": "8610012c5615a631c0bc",
+    "url": "/static/js/runtime~main.8610012c.js"
+  },
+  {
+    "revision": "f8131837c2738e784f620e4394705247",
+    "url": "/index.html"
+  }
+];
